1d88b234ab5b521f1b0bacc6f9d0ee96cb944949
laykkang <lekangimpl@foxmail.com>
2018-03-29 14:14:16 +0800

    注释语句

---
+++ pages/entry/entry.js
@@ -0,0 +1,63 @@
+// pages/mudol/mudol.js
+
+const toolbar = [
+    '../../images/nav/download.png', '../../images/nav/fav.png',
+    '../../images/nav/share.png', '../../images/nav/comment.png'
+];
+const app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 日志
+    diary:undefined,
+    // 顶部工具条
+    toolbar:toolbar,
+    // 图片预览模式
+    previewMode:false,
+    // 当前预览索引
+    previewIndex:0,
+    //多媒体内容列表
+    mediaList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (param) {
+      this.getDiares(param);
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+
+  getDiares(param){
+    var diaryData = app.data;
+    console.log('params :' + diaryData);
+    var id = param['id'];
+    // console.log(diaryData[id]);
+    console.log(diaryData[id].list);
+    if (id == undefined){
+        id = 0;
+    }
+    this.setData({
+        diary: diaryData[id]
+    })
+  },
+
+
+})
